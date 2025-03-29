d49b1faf502db4c2a3c99ec48ecfa44e79e5f787
0x00-V <dan@DESKTOP-O60KK2E>
2025-03-29 22:37:36 +0000

    Text to TextNodes

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,5 @@
+import re
+
 from enum import Enum
 from leafnode import LeafNode
 
@@ -52,3 +54,88 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                     else:
                         new_nodes.append(TextNode(content, text_type))
     return new_nodes
+
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        imgs = extract_markdown_images(node.text)
+        if not imgs:
+            new_nodes.append(node)
+            continue
+        
+        curr_txt = node.text
+
+        for alt, url in imgs:
+            img_markdown = f"![{alt}]({url})"
+            split = curr_txt.split(img_markdown, 1)
+            if split[0]:
+                new_nodes.append(TextNode(split[0], TextType.TEXT))
+            
+            new_nodes.append(TextNode(alt, TextType.IMAGE, url))
+
+            if len(split) > 1:
+                curr_txt = split[1]
+            else:
+                curr_txt = ""
+        if curr_txt:
+            new_nodes.append(TextNode(curr_txt, TextType.TEXT))
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        
+        links = extract_markdown_links(node.text)
+        if not links:
+            new_nodes.append(node)
+            continue
+        
+        curr_txt = node.text
+
+        for text, url in links:
+            link_markdown = f"[{text}]({url})"
+            split = curr_txt.split(link_markdown, 1)
+            if split[0]:
+                new_nodes.append(TextNode(split[0], TextType.TEXT))
+            
+            new_nodes.append(TextNode(text, TextType.LINK, url))
+
+            if len(split) > 1:
+                curr_txt = split[1]
+            else:
+                curr_txt = ""
+        
+        if curr_txt:
+            new_nodes.append(TextNode(curr_txt, TextType.TEXT))
+    return new_nodes
+
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    
+    return nodes
+    
