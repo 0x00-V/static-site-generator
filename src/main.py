05f0c283d29138eab9bd5e4395922f668226ab4e
0x00-V <0x00v@0x00v.com>
2025-04-04 00:22:00 +0100

    copytodst complete (Static copier)

--- src/main.py
+++ src/main.py
@@ -1,5 +1,26 @@
 from textnode import *
+import shutil, os
+
+
+def copytodst(static, public):
+    if os.path.exists(public):
+        shutil.rmtree(public)
+    os.mkdir(public)
+    
+    items = os.listdir(static)
+    for item in items:
+        static_path = os.path.join(static, item)
+        public_path = os.path.join(public, item)
+        if os.path.isfile(static_path):
+            print(f"FILE: {item} {static} -> {public}")
+            shutil.copy(static_path, public_path)
+        else:
+            print(f"Directory Created ({item}): {public}")
+            os.mkdir(public_path)
+            copytodst(static_path, public_path)
+
 
 if __name__ == "__main__":
+    copytodst("./static", "./public")
     node = TextNode("smth", TextType.BOLD)
     print(node)
