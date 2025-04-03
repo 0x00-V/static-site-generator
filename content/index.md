7357b99f2394d5966e4854f57098262217caae5c
0x00-V <0x00v@0x00v.com>
2025-04-04 00:59:05 +0100

    Single page loading complete

---
+++ content/index.md
@@ -0,0 +1,45 @@
+# Daniel's SSG
+
+![Some Dank Image](/images/tolkien.png)
+
+Here's the deal, **I am braindead**.
+
+> "Daniel's static site generator."
+
+
+## Blog posts
+
+- [Example 1](/blog/glorfindel)
+- [Example 2](/blog/tom)
+- [Example3](/blog/majesty)
+
+## Reasons I like Coding
+
+- Sigma
+- Sigma
+- Sigma
+- Sigma
+
+## My favorite words (in order)
+
+1. Sigma
+2. Sigma
+3. Sigma
+4. Sigma
+5. Sigma
+6. Sigma
+7. Sigma
+8. Sigma
+9. Sigma
+
+Here's what `brainfuck` looks like (the perfect coding language):
+
+```
+>+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-]
+>++++++++[<++++>-] <.>+++++++++++[<++++++++>-]<-.--------.+++
+.------.--------.[-]>++++++++[<++++>- ]<+.[-]++++++++++.
+```
+
+Want to get in touch? [No! Fuck off!](/contact).
+
+This site was generated with a custom-built [static site generator](https://www.boot.dev/courses/build-static-site-generator-python) made by Daniel Thomas
--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from textnode import *
 import shutil, os
 
-
 def copytodst(static, public):
     if os.path.exists(public):
         shutil.rmtree(public)
@@ -19,8 +18,35 @@ def copytodst(static, public):
             os.mkdir(public_path)
             copytodst(static_path, public_path)
 
+def extract_title(md):
+    md_lines = md.splitlines()
+    for line in md_lines:
+        if line.startswith("# "):
+            return line[2:].strip()
+    raise Exception("No h1 header found.")
+
+
+def generate_page(src, template, dst):
+    print(f"Generating page: {src} -> {dst} | Using: {template}")
+
+    with open(src, "r") as md_file:
+        markdown_content = md_file.read()
+    with open(template, "r") as template_file:
+        template_content = template_file.read()
+
+    html_node = markdown_to_html_node(markdown_content)
+    html_content = html_node.to_html()
+    title = extract_title(markdown_content)
+
+    html = template_content.replace("{{ Title }}", title)
+    html = html.replace("{{ Content }}", html_content)
+
+
+    os.makedirs(os.path.dirname(dst), exist_ok=True)
+    with open(dst, "w") as dst_file:
+        dst_file.write(html)
+    
 
 if __name__ == "__main__":
     copytodst("./static", "./public")
-    node = TextNode("smth", TextType.BOLD)
-    print(node)
+    generate_page("./content/index.md", "./template.html", "./public/index.html")
